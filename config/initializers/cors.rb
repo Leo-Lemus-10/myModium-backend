61736dbeb598e5008932a38f20e4893f0453d8e6
nmeek44 <nmeek44@gmail.com>
2021-06-22 15:34:07 -0400

    Commit message

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,6 +5,8 @@
 
 # Read more: https://github.com/cyu/rack-cors
 
+Rails.application.config.hosts << "http://localhost:3001/"
+
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins 'example.com'
